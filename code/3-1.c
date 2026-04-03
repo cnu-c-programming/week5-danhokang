51564ccef235c117c3ee8619b7e9382d49912073
danhokang <danho2898@naver.com>
2026-04-03 09:24:31 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -2,19 +2,16 @@
 
 void read_only(const int *arr, int size) {
 
-    // arr[0] = 999;  // ❌ 주석 제거하면 컴파일 에러 발생
+    // arr[0] = 999;  
 
     for (int i = 0; i < size; i++) {
         printf("%d ", *(arr + i));
     }
-
     printf("\n");
 }
 
 int main() {
     int nums[] = {10, 20, 30, 40, 50};
-
     read_only(nums, 5);
-
     return 0;
 }
