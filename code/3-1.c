b750e7c81007349fb6bdff7cdcd75a4634ef1f34
danhokang <danho2898@naver.com>
2026-04-03 09:28:30 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -5,13 +5,14 @@ void read_only(const int *arr, int size) {
     // arr[0] = 999;  
 
     for (int i = 0; i < size; i++) {
-        printf("%d ", *(arr + i));
+        printf("%d\n", *(arr + i));  
     }
-    printf("\n");
 }
 
 int main() {
     int nums[] = {10, 20, 30, 40, 50};
+
     read_only(nums, 5);
+
     return 0;
 }
